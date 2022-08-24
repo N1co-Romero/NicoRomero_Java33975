8a561e28d7e29f66c785c093d1bee50cbbc53301
nico.romero60@gmail.com <nico.romero60@gmail.com>
2022-08-24 17:22:48 -0300

    preentrega2

--- js/script.js
+++ js/script.js
@@ -1,132 +1,61 @@
-//CONSULTOR DE PRECIO
-/*
-const listasneakers = [
-    {
-        nombre: "jordan1",
-        precio: "2000$",
-        talle: "40",
-    },
-    {
-        nombre: "jordan1h",
-        precio: "2200$",
-        talle: "39",
-    },
-    {
-        nombre: "Nikeairmax ",
-        precio: "1800$",
-        talle: "43",
-    },
-    {
-        nombre: "Airforce1",
-        precio: "1000$",
-        talle: "44",
-    },
-    {
-        nombre: "Addidasyezzy",
-        precio: "2500$",
-        talle: "42",
-    },
-    {
-        nombre: "jordan4",
-        precio: "4000$",
-        talle: "40",
-    },
-]
 
+const items = document.getElementById('items')
+const templateCard = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
+let carrito = {}
 
-let Sneakers = ""
-while (Sneakers != "SALIR") {
-    Sneakers = prompt("ingrese CONSULTAR|COMPRAR|SALIR")
-    switch (Sneakers) {
-        case "CONSULTAR":
-            consultarprecios()
-            break;
-        case "COMPRAR":
-            comprasneakers()
-            break;
-        case "SALIR":
-            break;
-        default:
-            alert("ingrese una accion valida:")
-            Sneakers
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+    if (localStorage.getItem('carrito')) {
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        pintaCards()
+    }
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('api.json')
+        const data = await res.json()
+        //console.log(data)
+        pintaCards(data)
+    } catch (error) {
+        console.log(error)
     }
 }
-function consultarprecios() {
-    alert("Los Sneakers disponibles son: ")
-    listasneakers.forEach((sneaker) => {
-        alert(sneaker.nombre + " " + sneaker.precio + " talle disponible:" + sneaker.talle)
+
+items.addEventListener('click', e => {
+    addCarrito(e)
+})
+
+const pintaCards = data => {
+    data.forEach(prodcuto => {
+        templateCard.querySelector('h5').textContent = prodcuto.title
+        templateCard.querySelector('p').textContent = prodcuto.precio
+        templateCard.querySelector('img').setAttribute("src", prodcuto.thumbnailUrl);
+        templateCard.querySelector('.btn-dark').dataset.id = prodcuto.id
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+
     })
-
+    items.appendChild(fragment)
 }
-function comprasneakers() {
-    let compradesnkeakers = prompt("ingrese el snkeaker que desee:")
-    const snkeakerelegido = listasneakers.find((sneaker) => {
-        return sneaker.nombre == compradesnkeakers
-    })
-    if (snkeakerelegido) {
-        console.log("sneakers en el carrito")
-        console.log(snkeakerelegido)
-    } else {
-        {
-            console.log("no encontramos resultados")
-        }
+
+const addCarrito = e => {
+    //console.log(e.target)
+    //console.log(e.target.classlist.contains('btn-dark'))
+    if (e.target.classList.contains('btn-dark')) {
+        setCarrito(e.target.parentElement)
+
     }
+    e.stopPropagation()
 }
-*/
-//EVENTOS
-(function () {
-    //CONSTRUCTOR REMERAS
-    function Remeras(precio, talle) {
-        this.precio = precio
-        this.talle = talle
+
+const setCarrito = objeto => {
+    //console.log(objeto)
+    const prodcuto = {
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        title: objeto.querySelector('h5').textContent,
+        precio: objeto.querySelector('p').textContent
     }
-
-    let remera1 = new Remeras(500, "XL")
-    let remera2 = new Remeras(650, "L")
-    let remera3 = new Remeras(800, "S")
-
-    let agregar1 = function () {
-        alert("haz añadido el producto a tu carrito")
-    }
-
-    let boton1 = document.getElementById('boton1');
-    boton1.addEventListener("click", agregar1)
-
-    let info1 = function () {
-        alert("Remera gucci $" + remera1.precio + " talle: " + remera1.talle)
-    }
-
-    let boton4 = document.getElementById('boton4');
-    boton4.addEventListener("click", info1)
-
-    let agregar2 = function () {
-        alert("haz añadido el producto a tu carrito")
-    }
-
-    let boton2 = document.getElementById('boton2');
-    boton2.addEventListener("click", agregar2)
-
-    let info2 = function () {
-        alert("Remera gucci tiger $" + remera2.precio + " talle: " + remera2.talle)
-    }
-
-    let boton5 = document.getElementById('boton5');
-    boton5.addEventListener("click", info2)
-
-    let info3 = function () {
-        alert("Remera gucci wolf $" + remera3.precio + " talle: " + remera3.talle)
-    }
-
-    let boton6 = document.getElementById('boton6');
-    boton6.addEventListener("click", info3)
-
-    let agregar3 = function () {
-        alert("haz añadido el producto a tu carrito")
-    }
-    let boton3 = document.getElementById('boton3');
-    boton3.addEventListener("click", agregar3)
-})()
-
-
-
-
+    console.log(prodcuto)
+}
